2ea1a82c520354905311580ed8c417a923328c21
katayama <tattu.0310@gmail.com>
2022-07-31 13:50:51 +0900

    Revert "🔥 fire"
    This reverts commit 07a5d9b556ed1d9f187531af2a2940b93b8954f8.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ const App: CustomAppPage = ({ Component, pageProps }) => {
     <GlobalStyleProvider>
       <AppMantineProvider>
         <NotificationsProvider position="bottom-left">
-          <main role="main">
+          <main role="main" className="bg-red-500">
             {getLayout(<Component {...pageProps} />)}
           </main>
         </NotificationsProvider>
