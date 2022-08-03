589c768699af43a5a97c04ba5ba9b36d8a627746
katayama <tattu.0310@gmail.com>
2022-08-03 22:24:48 +0900

    ✨ table作成

--- src/pages/administration/memberedit.tsx
+++ src/pages/administration/memberedit.tsx
@@ -1,11 +1,74 @@
 import React from "react";
 import { DashboardLayout } from "src/layout";
+import { Table } from "@mantine/core";
+import { PageContainer } from "src/component/PageContainer";
+import { PageContent } from "src/component/PageContent";
 
 const MemberEdit = () => {
+  const elements = [
+    {
+      position: "一般",
+      number: 1000100072,
+      name: "片山達文",
+      email: "t-katayama@gmail.com",
+    },
+    {
+      position: "一般",
+      number: 1000100072,
+      name: "片山達文",
+      email: "t-katayama@gmail.com",
+    },
+    {
+      position: "一般",
+      number: 1000100072,
+      name: "片山達文",
+      email: "t-katayama@gmail.com",
+    },
+    {
+      position: "一般",
+      number: 1000100072,
+      name: "片山達文",
+      email: "t-katayama@gmail.com",
+    },
+    {
+      position: "一般",
+      number: 1000100072,
+      name: "片山達文",
+      email: "t-katayama@gmail.com",
+    },
+  ];
+
+  const rows = elements.map((element) => (
+    <tr key={element.name}>
+      <td>{element.name}</td>
+      <td>{element.position}</td>
+      <td>{element.number}</td>
+      <td>{element.email}</td>
+    </tr>
+  ));
   return (
     <div>
-      <div>memberedit</div>
-      <div>管理者だけがいじれるメンバー管理ページ</div>
+      <PageContainer title="memberedit">
+        <PageContent className="w-[800px] m-auto">
+          <Table
+            horizontalSpacing="xl"
+            verticalSpacing="lg"
+            fontSize="md"
+            highlightOnHover
+            className="text-center"
+          >
+            <thead>
+              <tr>
+                <th>名前</th>
+                <th>役職</th>
+                <th>従業員番号</th>
+                <th>メールアドレス</th>
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </Table>
+        </PageContent>
+      </PageContainer>
     </div>
   );
 };
