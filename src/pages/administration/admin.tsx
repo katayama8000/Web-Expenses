65413e1b08aff5b804319aa6b8585ec055a780c4
katayama <tattu.0310@gmail.com>
2022-08-06 00:23:13 +0900

    💄 UI向上委員会

--- src/pages/administration/admin.tsx
+++ src/pages/administration/admin.tsx
@@ -100,23 +100,3 @@ const Admin = () => {
 
 Admin.getLayout = DashboardLayout;
 export default Admin;
-
-// function Demo() {
-//   const [opened, setOpened] = useState(false);
-
-//   return (
-//     <>
-//       <Modal
-//         opened={opened}
-//         onClose={() => setOpened(false)}
-//         title="Introduce yourself!"
-//       >
-//         {/* Modal content */}
-//       </Modal>
-
-//       <Group position="center">
-//         <Button onClick={() => setOpened(true)}>Open Modal</Button>
-//       </Group>
-//     </>
-//   );
-// }
