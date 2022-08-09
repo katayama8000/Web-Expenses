6c1a9c90f349da859624f4531d4f4f20b5f70c61
katayama <tattu.0310@gmail.com>
2022-08-09 17:59:00 +0900

    ⚡️ storage test

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,31 +1,76 @@
 import { Button } from "@mantine/core";
-import { DashboardLayout } from "src/layout";
+import React, { useState } from "react";
 import { supabase } from "src/lib/supabase/supabase";
-import { state } from "src/lib/state/state";
-import { useAtom } from "jotai";
+const Profile = () => {
+  const [image, setImage] = useState<File | undefined>();
+  const [username, setUsername] = useState("");
+  const [website, setWebsite] = useState("");
+  const [avatarUrl, setAvatarUrl] = useState<File | undefined>();
+  const [message, setMessage] = useState("");
 
-export function Test() {
-  const [data, setData] = useAtom(state);
-  const handleTest = async () => {
-    const { data, error } = await supabase
-      .from("test")
-      .insert([{ name: "test" }]);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    let avatarUrl = "";
 
-    console.log(data, error);
+    if (image) {
+      const { data, error } = await supabase.storage
+        .from("avatars")
+        .upload(`${Date.now()}`, image);
+
+      if (error) {
+        console.log(error);
+      }
+    }
   };
+
   return (
     <div>
-      <div>aaa</div>
-      <div>
-        {data.map((item) => (
-          <div key={item.name}>{item.name}</div>
-        ))}
-      </div>
-      <Button onClick={handleTest}>test</Button>
+      {message && message}
+
+      <h1>Profile</h1>
+
+      {avatarUrl ? (
+        <img
+          src={`https://rmzhnvojmpqewraookpz.supabase.in/storage/v1/object/public/${avatarUrl}`}
+          width={200}
+          alt=""
+        />
+      ) : (
+        "No Avatar set"
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="avatar">Choose Avatar:</label>
+          <input
+            type="file"
+            accept={"image/jpeg image/png"}
+            onChange={(e) => setImage(e.target.files?.[0])}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="website">Website:</label>
+          <input
+            type="text"
+            onChange={(e) => setWebsite(e.target.value)}
+            value={website}
+          />
+        </div>
+
+        <Button type={"submit"}>Save profile!</Button>
+      </form>
     </div>
   );
-}
+};
 
-Test.getLayout = DashboardLayout;
-
-export default Test;
+export default Profile;
