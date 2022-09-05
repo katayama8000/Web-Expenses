81061d0117e58c6e64fb102cec7c3b04df1f02b9
katayama <tattu.0310@gmail.com>
2022-09-06 00:05:49 +0900

    🐛 fix tailwind version up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false,
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
